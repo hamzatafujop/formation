246e4633157e30d080f699a1f2120257a3cc3a5e
kilani hamza <kilanihamza89@gmail.com>
2022-05-06 15:45:11 +0100

    second edit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends AbstractController
     public function index(): Response
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Welcome to your new controller! twoooooooo',
             'path' => 'src/Controller/ArticleController.php',
         ]);
     }
